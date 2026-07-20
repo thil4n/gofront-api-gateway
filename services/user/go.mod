4717d8ae3f8b8154b9783f8f3292d783fadf11c1
Thilan Dissanayaka <thil4n@gmail.com>
2026-07-20 21:35:24 +0530

    Add dummy server

---
+++ services/user/go.mod
@@ -0,0 +1,3 @@
+module services/user
+
+go 1.25.2
---
+++ services/user/main.go
@@ -0,0 +1,18 @@
+type user struct {
+	id int 
+	name string
+	age int
+}
+
+
+var users = []user {
+	{
+	id: 1,
+	name: "Alice"
+	},
+	{
+	id: 2,
+	name: "Bob"
+	}
+
+}

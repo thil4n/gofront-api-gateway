0d681cd0863acc60eaf676d94867d958a163be0e
Thilan Dissanayaka <thil4n@gmail.com>
2026-09-24 23:04:01 +0530

    Introduce Http Mux

--- cmd/services/user/main.go
+++ cmd/services/user/main.go
@@ -8,14 +8,31 @@ import (
 
 
 
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!\n")
 }
 
 
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Echo: Hello World!\n")
+}
+
+
+
 func main(){
-	http.HandleFunc("/hello", hello)
+	mux := http.NewServeMux()
+
+    mux.HandleFunc("/hello", hello	)
+    mux.HandleFunc("/echo", echo)
+
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+
+	}
 
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(server.ListenAndServe())
 }
